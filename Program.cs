a8d667123255f2c1d692cce4d64295202006b300
Shreya@18 <shreyantasulebhavi1gmail.com>
2022-01-16 23:26:46 +0530

    [shreya] Day4Assignment

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SnakeAndladder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Snakes & Ladders Games");
+            SnakeLadder snakeLadder = new SnakeLadder();
+            snakeLadder.Game();
+           
+            
+        }
+    }
+}
